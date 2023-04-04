4093b8573b739b9fa3dac14a805cac4c3fe36871
ybwell <2350885331@qq.com>
2023-04-04 19:03:07 +0800

    features

--- deploy.js
+++ deploy.js
@@ -6,7 +6,9 @@ require("dotenv").config()
 async function main() {
     // First, compile this!
     // And make sure to have your ganache network up!
-    let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    // On ether 6 and above, you should use like this
+    let provider = new ethers.JsonRpcProvider(process.env.RPC_URL)
+    // let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
     let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
     // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
     // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
